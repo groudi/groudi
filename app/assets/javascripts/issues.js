92c26f38e461e799b53a120a51a2499cdb05fa6b
macbook <macbook@riverbed.local>
2017-01-12 13:44:29 +0100

    question changed for feedback

--- app/assets/javascripts/issues.js
+++ app/assets/javascripts/issues.js
@@ -58,7 +58,7 @@ var readyDataSource = function() {
 	});
 
   $(".btn-feed").click(function(){
-    window.location.href = "/feedback";
+    window.location.href = window.location.protocol + "//" + window.location.host + "/feedback";
   });
 
 	$(document).on('click', '.btn-add-attribute', function(e)
